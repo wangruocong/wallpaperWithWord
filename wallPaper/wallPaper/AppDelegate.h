c072c86aafe2b3a5db325e6f1dd63ab3bede886e
wangruocong <wangruocong@outlook.com>
2016-11-19 18:14:25 +0800

    first

---
+++ wallPaper/wallPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wallPaper
+//
+//  Created by Roy on 2016/11/19.
+//  Copyright © 2016年 wangruocong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
